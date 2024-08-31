61720036f9eb5f8a22b43dcc07f67e96ce02ff7a
Serkankaya08 <kayaserkan0838@gmail.com>
2024-08-31 20:22:43 +0300

    Update kodland1.py

--- kodland1.py
+++ kodland1.py
@@ -1,3 +1,15 @@
 rastgele_gercekler=["Serkan","python basic projem","tost","kod yazmak"]
 rastgele_gercek= random.choice(rastgele_gercekler)
 print("Hakkımdaki rastgele gerçek: ", rastgele_gercek)
+
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+            
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+if word in meme_dict.keys():
+    print("Aradığınız kelimenin anlamı: ", meme_dict[word])
+else:
+    print("Aradığınız kelime bulunamamaktadır.")
